225d76e214271ad340aea923ef0750ad85e3472d
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-09-20 16:46:46 +0100

    extra routes added

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World from Levi!')) 
 
+app.get('/bananas', (req, res) => 
+
+  res.send('hello world, this is bananas')); 
+
+app.get('/fruit/apples', (req, res) => 
+
+  res.send('hello world, this is apples ')); 
  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
